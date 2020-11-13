0d4fecd77f0b627e02a9f64017cf571e4b2a31e6
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-13 12:35:50 +0530

    relative list created

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    this.state = { relativeList: ["relative1", "relative2", "relative3"] };
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {this.state.relativeList.map((relatives,index) => (
+            <li key={`relativeListItem${index+1}`}>{relatives}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
